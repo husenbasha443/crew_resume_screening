d114f768cf949cd64e935553cc96563d0f88a303
Husen Basha <23ca032.husen@sjec.ac.in>
2026-01-09 20:54:28 +0530

    Update crew.py

--- src/crewai_resume_screening/crew.py
+++ src/crewai_resume_screening/crew.py
@@ -19,39 +19,39 @@ class CrewaiResumeScreening():
     @agent
     def job_analyst_agent(self) -> Agent:
         return Agent(
-            config=self.agents_config['job_analyst_agent'], # type: ignore[index]
+            config=self.agents_config['job_analyst_agent'], 
             verbose=True
         )
 
     @agent
     def skill_matching_agent(self) -> Agent:
         return Agent(
-            config=self.agents_config['skill_matching_agent'], # type: ignore[index]
+            config=self.agents_config['skill_matching_agent'], 
             verbose=True
         )
     @agent
     def interview_planner_agent(self) -> Agent:
         return Agent(
-            config=self.agents_config['interview_planner_agent'], # type: ignore[index]
+            config=self.agents_config['interview_planner_agent'], 
             verbose=True
         )
 
     @task
     def job_analysis_task(self) -> Task:
         return Task(
-            config=self.tasks_config['job_analysis_task'], # type: ignore[index]
+            config=self.tasks_config['job_analysis_task'], 
         )
 
     @task
     def skill_matching_task(self) -> Task:
         return Task(
-            config=self.tasks_config['skill_matching_task'], # type: ignore[index]
+            config=self.tasks_config['skill_matching_task'], 
            
         )
     @task
     def interview_question_task(self) -> Task:
         return Task(
-            config=self.tasks_config['interview_question_task'], # type: ignore[index]
+            config=self.tasks_config['interview_question_task'], 
             output_file = "blogs/blog.md"
         )
 
@@ -60,10 +60,10 @@ class CrewaiResumeScreening():
         """Creates the CrewaiResumeScreening crew"""
 
         return Crew(
-            agents=self.agents, # Automatically created by the @agent decorator
-            tasks=self.tasks, # Automatically created by the @task decorator
+            agents=self.agents, 
+            tasks=self.tasks, 
             process=Process.sequential,
             verbose=True,
             trace=True,
-            # process=Process.hierarchical, # In case you wanna use that instead https://docs.crewai.com/how-to/Hierarchical/
+            # process=Process.hierarchical, 
         )
